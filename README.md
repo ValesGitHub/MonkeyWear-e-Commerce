6d3fc7d2d1e8a2c07b74424eb246c994d24f2776
Valerio Pesacane <pesacanevalerio@gmail.com>
2022-06-08 16:07:46 +0200

    feat: readme and ts

---
+++ README.md
@@ -0,0 +1,10 @@
+# MonkeyWear
+
+**Version 0.0.0**
+
+A Fashion E-Commerce
+
+---
+
+## install dependencies
+yarn 
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { getCookie } from "./scripts/cookiescript.js";
+
+if(document.cookie) {
+    if(getCookie("gender") == 'M') {
+        location.href = "manpage.html";
+    }
+    else {
+        location.href = "womanpage.html";
+    }
+}
+
+document.body.onload = () => {
+    let womanBtn = document.querySelector<HTMLDivElement>(".woman-select");
+    let manBtn = document.querySelector<HTMLDivElement>(".man-select");
+
+    if(womanBtn && manBtn) {
+      womanBtn.addEventListener("click", () => {
+        location.href = "womanpage.html";
+      });
+      
+      manBtn.addEventListener("click", () => {
+          location.href = "manpage.html";
+      });
+    }
+}
+
+
